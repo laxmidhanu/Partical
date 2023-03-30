1302c8f0e3877044b769eb2ab8a781aef5012158
laxmidhanu <127470276+laxmidhanu@users.noreply.github.com>
2023-03-30 20:11:31 +0530

    Add files via upload

---
+++ USER_DEFINES.java
@@ -0,0 +1,16 @@
+package method;
+
+public class USER_DEFINES 
+{
+	
+		public int add(int a, int b)
+		{
+			return a+b;
+		}
+public static void main(String args [])
+{
+	USER_DEFINES obj = new USER_DEFINES();
+	System.out.println(obj.add(20,3));
+}
+
+}
